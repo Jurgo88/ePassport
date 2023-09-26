9405f2e4fae37c0e5ef759edeed8f6171e66c69d
Jurgo <jurgo88@gmail.com>
2023-09-26 03:07:07 +0200

    Predeparture checklist fixed

--- src/pages/Predeparture/BeforeProjectChecklistPage.vue
+++ src/pages/Predeparture/BeforeProjectChecklistPage.vue
@@ -1,37 +1,76 @@
 <script setup>
-import { ref, computed, watch } from 'vue';
+import { ref, computed, onBeforeMount } from 'vue';
+import { useStore } from 'vuex';
+import { loadVolunteerDataByID } from '/services/database.js';
+import sendData from '../../../services/sendData.js';
 
 const selectedAll = ref(false);
-
-// const selected = ref([]);
-
-// const selectedLength = computed(() => selected.length);
-
-// console.log('selectedLength: ' + selectedLength.value);
+const store = useStore();
+const userState = computed(() => store.state.auth.userDetails);
+const uid = userState.value.userData.email;
+const volunteerData = ref({});
+const loading = ref(false);
+const hasChecklist = ref(false); 
 
 // Create an array to store checkbox values
-const checkboxes = ref([]);
+const checkboxes = ref([false, false, false , false, false, false, false, false, false, false, false, false, false, false, false, false, false, false , false, false, false, false, false, false]);
 
 // Compute whether all checkboxes are selected
 const allCheckboxesSelected = computed(() => {
-  return checkboxes.value.every(checkbox => checkbox);
+  for (let i = 0; i < checkboxes.value.length; i++) {
+    if (!checkboxes.value[i]) {
+      return false;
+    }
+  }
+  console.log('allCheckboxesSelected computed true');
+  selectedAll.value = true;
+  nextButtonColor.value = 'success';
+  return true;
 });
 
-// Watch for changes in allCheckboxesSelected and update the button color
-watch(allCheckboxesSelected, (newValue) => {
-  if (newValue) {
-    nextButtonColor.value = 'secondary';
-  } else {
-    nextButtonColor.value = 'accent';
+
+// Button color
+const nextButtonColor = ref('error');
+
+function checkAllCheckboxes () {
+  for (let i = 0; i < checkboxes.value.length; i++) {
+    checkboxes.value[i] = true;
+  }
+}
+
+function controlChecklistFromDatabase() {
+
+  if(hasChecklist.value)
+  {
+    nextButtonColor.value = 'success';
+    checkAllCheckboxes();
+  }
+}
+
+function saveChecklist() {
+  volunteerData.value.beforeProject.checklist = true;
+  sendData(volunteerData.value);
+}
+
+onBeforeMount(async () => {
+  try {
+    const dataFromDatabase = await loadVolunteerDataByID(uid);
+    volunteerData.value = dataFromDatabase; // Meníme hodnotu ref.
+    loading.value = true;
+    console.log('volunteerData.value.beforeProject.checklist: ' + volunteerData.value.beforeProject.checklist);
+    if(volunteerData.value.beforeProject.checklist)
+    {
+      hasChecklist.value = true;
+      controlChecklistFromDatabase();
+    }
+  } catch (error) {
+    console.log(error);
   }
 });
 
-// Button color
-const nextButtonColor = ref('accent');
-
 </script>
 <template>
-    <v-container>
+    <v-container v-if="loading">
         <h1>Before departure</h1>
         <br>
         <p>
@@ -46,13 +85,13 @@ const nextButtonColor = ref('accent');
         <h3>BANK</h3>
         <br>
         <v-checkbox v-model="checkboxes[2]" label="Check if your credit card works abroad. Check the potential extra fees to withdraw cash abroad."></v-checkbox>
-        <v-checkbox v-model="selected" label="Check the exchange rate costs with your bank."></v-checkbox>
-        <v-checkbox v-model="selected" label="Call your bank adviser to inform him/her about your stay abroad and to avoid your credit card being blocked. This call could be the opportunity to negotiate some options to reduce the extra fees."></v-checkbox>
+        <v-checkbox v-model="checkboxes[3]" label="Check the exchange rate costs with your bank."></v-checkbox>
+        <v-checkbox v-model="checkboxes[4]" label="Call your bank adviser to inform him/her about your stay abroad and to avoid your credit card being blocked. This call could be the opportunity to negotiate some options to reduce the extra fees."></v-checkbox>
         <br>
         <h3>COMMUNICATION</h3>
         <br>
-        <v-checkbox v-model="selected" label="Check if your European mobile operator works abroad and/or at home."></v-checkbox>
-        <v-checkbox v-model="selected" label="Prepare and always keep with you a list (in your phone AND on paper) with the following information: Emergency numbers of the Hosting country (Police, Firemen, Medical emergencies, etc.) Sending Organisation, Project manager (email, phone numbers) Hosting organisation, Mentor (email, phone numbers, address) Home country Embassy (email, phone number, address) o Personal emergency contacts (email, phone numbers) Hospital (phone numbers, address) Insurance (email, phone numbers) Etc."></v-checkbox>
+        <v-checkbox v-model="checkboxes[5]" label="Check if your European mobile operator works abroad and/or at home."></v-checkbox>
+        <v-checkbox v-model="checkboxes[6]" label="Prepare and always keep with you a list (in your phone AND on paper) with the following information: Emergency numbers of the Hosting country (Police, Firemen, Medical emergencies, etc.) Sending Organisation, Project manager (email, phone numbers) Hosting organisation, Mentor (email, phone numbers, address) Home country Embassy (email, phone number, address) o Personal emergency contacts (email, phone numbers) Hospital (phone numbers, address) Insurance (email, phone numbers) Etc."></v-checkbox>
         <br>
         <div style="padding-left: 2em;">
             <ul>
@@ -70,33 +109,38 @@ const nextButtonColor = ref('accent');
         <br>
         <h3>HEALTH</h3>
         <br>
-        <v-checkbox v-model="selected" label="Validate your insurance registration by creating your personal space (just follow the instructions in the emails you will receive)."></v-checkbox>
-        <v-checkbox v-model="selected" label="Download and print your membership card, if any. Save it too in your emails."></v-checkbox>
-        <v-checkbox v-model="selected" label="Check if all the vaccines have been done."></v-checkbox>
-        <v-checkbox v-model="selected" label="In case of any specific medical needs, inform your doctor about your departure and find information about the availability of the medicines you need in your hosting country."></v-checkbox>
-        <v-checkbox v-model="selected" label="Inform your Social Insurance and your complementary insurance about your departure."></v-checkbox>
+        <v-checkbox v-model="checkboxes[7]" label="Validate your insurance registration by creating your personal space (just follow the instructions in the emails you will receive)."></v-checkbox>
+        <v-checkbox v-model="checkboxes[8]" label="Download and print your membership card, if any. Save it too in your emails."></v-checkbox>
+        <v-checkbox v-model="checkboxes[9]" label="Check if all the vaccines have been done."></v-checkbox>
+        <v-checkbox v-model="checkboxes[10]" label="In case of any specific medical needs, inform your doctor about your departure and find information about the availability of the medicines you need in your hosting country."></v-checkbox>
+        <v-checkbox v-model="checkboxes[11]" label="Inform your Social Insurance and your complementary insurance about your departure."></v-checkbox>
         <br>
         <h3>SAFETY</h3>
         <br>
-        <v-checkbox v-model="selected" label="Find information about the Hosting country situation. Check your Home Country’s Ministry of Foreign Affairs. They should have some advices to follow."></v-checkbox>
-        <v-checkbox v-model="selected" label="Do not forget to register yourself at your arrival in your Home Country Embassy."></v-checkbox>
+        <v-checkbox v-model="checkboxes[12]" label="Find information about the Hosting country situation. Check your Home Country’s Ministry of Foreign Affairs. They should have some advices to follow."></v-checkbox>
+        <v-checkbox v-model="checkboxes[13]" label="Do not forget to register yourself at your arrival in your Home Country Embassy."></v-checkbox>
         <br>
         <h3>PRACTICAL INFORMATION</h3>
         <br>
-        <v-checkbox v-model="selected" label="Check if you need a plug adaptor."></v-checkbox>
-        <v-checkbox v-model="selected" label="Think to bring with you some adapted clothes to work onsite. If needed, you can also bring some specific material to realise your tasks. Ask your hosting organisation."></v-checkbox>
-        <v-checkbox v-model="selected" label="Check if someone will pick you up at the airport or find out about local transports at your arrival."></v-checkbox>
-        <v-checkbox v-model="selected" label="Get prepared for the jetlag."></v-checkbox>
+        <v-checkbox v-model="checkboxes[14]" label="Check if you need a plug adaptor."></v-checkbox>
+        <v-checkbox v-model="checkboxes[15]" label="Think to bring with you some adapted clothes to work onsite. If needed, you can also bring some specific material to realise your tasks. Ask your hosting organisation."></v-checkbox>
+        <v-checkbox v-model="checkboxes[16]" label="Check if someone will pick you up at the airport or find out about local transports at your arrival."></v-checkbox>
+        <v-checkbox v-model="checkboxes[17]" label="Get prepared for the jetlag."></v-checkbox>
         <br>
         <h3>DO NOT FORGET also</h3>
         <br>
-        <v-checkbox v-model="selected" label="To bring a present to your host family and hosting organization (chocolate are the best present for all)"></v-checkbox>
-        <v-checkbox v-model="selected" label="Inform your sending organization and your family that you have arrived."></v-checkbox>
-        <v-checkbox v-model="selected" label="Send a scan of your boarding pass to your sending organization. Keep all the boarding passes and travel tickets in original – you will need to send them to your SO at the end of the project!"></v-checkbox>
-        <v-checkbox v-model="selected" label="Gather the information regarding your results in the field."></v-checkbox>
-        <v-checkbox v-model="selected" label="Complete and send the statistics table (here) to your sending organization. As your deployment is financed by European funds, it is our responsibility as sending organization to be accountant and report to the donor"></v-checkbox>
-        <v-checkbox v-model="selected" label="During all the duration of your project keep a track of your activities: photos, attendance sheets from training that you have carried out, events... and send them to your sending organization..."></v-checkbox>
+        <v-checkbox v-model="checkboxes[18]" label="To bring a present to your host family and hosting organization (chocolate are the best present for all)"></v-checkbox>
+        <v-checkbox v-model="checkboxes[19]" label="Inform your sending organization and your family that you have arrived."></v-checkbox>
+        <v-checkbox v-model="checkboxes[20]" label="Send a scan of your boarding pass to your sending organization. Keep all the boarding passes and travel tickets in original – you will need to send them to your SO at the end of the project!"></v-checkbox>
+        <v-checkbox v-model="checkboxes[21]" label="Gather the information regarding your results in the field."></v-checkbox>
+        <v-checkbox v-model="checkboxes[22]" label="Complete and send the statistics table (here) to your sending organization. As your deployment is financed by European funds, it is our responsibility as sending organization to be accountant and report to the donor"></v-checkbox>
+        <v-checkbox v-model="checkboxes[23]" label="During all the duration of your project keep a track of your activities: photos, attendance sheets from training that you have carried out, events... and send them to your sending organization..."></v-checkbox>
         <br>
+
+        <!-- Sellect all for test purpose -->
+        <v-checkbox v-model="selectedAll" label="Select all" @change="checkAllCheckboxes"></v-checkbox>
+
+        <v-btn @click="saveChecklist" :color="nextButtonColor" class="my-button" :disabled="!allCheckboxesSelected">Save</v-btn>
         <hr>
         <br>
         <router-link :to="{
@@ -104,9 +148,7 @@ const nextButtonColor = ref('accent');
         }">
         <v-btn color="accent" class="my-button">Back</v-btn>
 
-        </router-link>
-     
-
+        </router-link> 
          
     </v-container>
 </template>
